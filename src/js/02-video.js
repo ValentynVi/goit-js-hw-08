a56bd048c8450e7fc69c77a8e3085fb27cd36c0a
ValentynVi <123833351+ValentynVi@users.noreply.github.com>
2023-08-13 18:31:17 +0100

    add code file 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,5 +4,18 @@ import throttle from 'lodash.throttle';
 
 
 const iframe = document.querySelector('iframe');
+// console.log(iframe);
 const player = new Player(iframe);
-console.log(player);
+// console.log(player);
+player.on('timeupdate', throttle(fnTime, 1000));
+
+function fnTime(data) {
+    console.log(data);
+    let time = data.seconds;
+    console.log(time);
+    localStorage.setItem('videoplayer-current-time', time);
+};
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+// console.log(currentTime);
+player.setCurrentTime(currentTime);
